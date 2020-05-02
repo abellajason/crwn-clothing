994e40f629f9bdbd6e0c3af46eb7581e199f0c49
jason <jason@snapzio.com>
2020-05-03 04:46:27 +0800

    moved our directory and shop data into its onw reducer, careted respective selectors and updated corresponding components with new reduxflow for directory/shop

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store';
-
 import {BrowserRouter} from 'react-router-dom';
+import {PersistGate} from 'redux-persist/integration/react';
+
+import { store, persistor } from './redux/store';
+
+
 import App from './App';
 import './index.css';
 
@@ -11,7 +14,9 @@ import './index.css';
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
+        <PersistGate persistor={persistor}>
             <App />
+        </PersistGate>
         </BrowserRouter>
     </Provider>,
     document.getElementById('root')
