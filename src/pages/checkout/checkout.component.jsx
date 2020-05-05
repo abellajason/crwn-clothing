054891d767d5dba4581030cc8a99a8bcfd6fd99e
jason <jason@snapzio.com>
2020-05-05 20:07:19 +0800

    updating stripe integration and updating our favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -5,7 +5,8 @@ import {createStructuredSelector} from 'reselect';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors'
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
-import './checkout.styles.scss'
+import './checkout.styles.scss';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component'
 
 
 const CheckoutPage = ({cartItems, total}) => (
@@ -36,6 +37,12 @@ const CheckoutPage = ({cartItems, total}) => (
         <div className="total">
             <span>TOTAL: $ {total}</span>
         </div>
+        <div className="test-warning">
+            *Please use the following test credit card for payments* 
+            <br/>
+            4242 4242 4242 4242 - Exp: 05/20 - CVV: 123
+        </div>
+        <StripeCheckoutButton price={total}/>
     </div>
 )
 
